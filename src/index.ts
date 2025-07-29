67b0a1917da2139256a06f1fce713273b111e888
Giovanni Campeol <giovanni@asus-giovanni.wind3.hub>
2025-07-29 10:50:01 +0200

    improvements

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
+#!/usr/bin/env node
+
 import { Command } from "commander"
 import { deck } from "./deck.js"
 import { llm } from "./llm.js"
 import { word } from "./word.js"
 import { phrase } from "./phrase.js"
 
-const cli = new Command("zzcli")
+const cli = new Command("hzcli")
 
-cli.addCommand(llm)
+cli.addCommand(deck)
 cli.addCommand(word)
 cli.addCommand(phrase)
-cli.addCommand(deck)
+cli.addCommand(llm)
 
 cli.parse()
